e0ac58a26336b69cd11427b09e26a4c89a427a85
Kevin Maurizi <kevin.maurizi@bzz.ch>
2023-12-04 14:30:50 +0100

    init

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-def calculate():
-    """
-    calculates the total
-    :return: None
-    """
-    price = 15.75
-    quantity = 3  # TODO change quantity to 5
-    print(f'Total: {price * quantity}')
+from input_reader import read_int, read_float
+def main():
+    # do something to show functionality
+    mark = read_float('Please enter a mark for your python class> ', lower_bound= 0, upper_bound= 6)
+    score_0_to_100 = read_int('Please enter a whole number between 0 and 100> ', lower_bound = 0, upper_bound = 100)
+    positive_zahl = read_int('Please enter a positive whole number> ', lower_bound = 1)
+    negative_zahl = read_int('Please enter a negative whole number> ', upper_bound = -1)
+
+    print('Your mark is: ', mark)
+    print('Your rating is: ', score_0_to_100)
+    print('Your positive number is: ', positive_zahl)
+    print('Your negative number is: ', negative_zahl)
 
 
 if __name__ == '__main__':
-    calculate()
+    main()
